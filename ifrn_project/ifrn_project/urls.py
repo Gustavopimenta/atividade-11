10d1ba2e3d987a93c14ce02db58e10eb08ee0532
Gustavo Pimenta <gustav.alves17@gmail.com>
2023-10-27 17:07:17 -0300

    gitignore

--- ifrn_project/ifrn_project/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('lista/', views.lista_alunos, name='lista_alunos'),
-    path('editar/<int:aluno_id>/', views.editar_aluno, name='editar_aluno'),
-    path('salvar/<int:aluno_id>/', views.salvar_alteracoes, name='salvar_alteracoes'),
-    
-]
